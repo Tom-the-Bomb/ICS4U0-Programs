5608c1dc8fbb05bd2a79abe9a93f2b7c2f5f407d
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2024-09-25 18:20:13 -0400

    z

--- SampleTest1/TimeConverter.java
+++ SampleTest1/TimeConverter.java
@@ -17,8 +17,5 @@ public class TimeConverter {
             seconds % 60
         );
         sc.close();
-
-        double a = 2.2, b = 3.5, c = 0.8;
-        System.out.println((Math.pow(b, 2) + Math.sqrt(Math.pow(b,2) + 2*a*c)) / (2*a + 3));
     }
 }
